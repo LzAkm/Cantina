0437c6d1441e886d29edb6b5bee92566a9fd5774
LzAkm <louiza.aitkacem@hotmail.com>
2023-09-18 20:27:14 +0200

    Modification de la navbar et stylisation

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,13 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
-
+import '../Styles/Navbar.css'
+/* import logo from '../../public/img/cantina-logo.png'; */
 
 function Navigationbar() {
     return (
         <div className='navbar'>
-            <ul>
-                <li><a href="">Home</a></li>
-            </ul>
+            {/* <img src={ logo } alt="" /> */}
+            <h3>Homepage</h3>
             <button className='add-btn'>ajouter une recette <FontAwesomeIcon icon={faPlus} /></button>
         </div>
     );
