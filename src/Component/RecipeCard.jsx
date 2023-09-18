15a314c058bf0afd0f474cde7b26005a31844843
LzAkm <louiza.aitkacem@hotmail.com>
2023-09-18 17:59:17 +0200

    Dynamisation des données dures

--- src/Component/RecipeCard.jsx
+++ src/Component/RecipeCard.jsx
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchRecipeData } from '../services/api.jsx';
 import '../Styles/RecipeCard.css';
-import test from './plat-test.png'; 
 
 function RecipeCard() {
+  const [recipeData, setRecipeData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Utilisez la fonction d'API pour récupérer les données de la recette
+    const fetchData = async () => {
+      try {
+        const data = await fetchRecipeData();
+        console.log(data);
+        setRecipeData(data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Erreur lors de la récupération des données de la recette :', error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className='recipe-card'>
-      <img className='recipe-img' src={ test } alt='' /> 
+      <img className='recipe-img' src={recipeData.photo} alt='' /> 
       <div className='recipe-body'>
-        <h2>Recipe title</h2>
-        <p>Navarin d'agneau, quinoa, légumes glacés et jus réduit épicé</p>
-        <p>Niveau de difficulté : jedi</p>
+        <h2>{recipeData.titre}</h2>
+        <p>{recipeData.description}</p>
+        <p>Niveau de difficulté : {recipeData.niveau}</p>
         <hr className='separator' />
         <div className='recipes-info'>
             <div className='prep-time'>
                 <p className='grey'>Temps</p>
-                <p className='bold'>45 min</p>
+                <p className='bold'>{recipeData.tempsPreparation} min</p>
             </div>
             <div className='proportion'>
                 <p className='grey'>Proportion</p>
-                <p className='bold'>6 personnes</p>
+                <p className='bold'>{recipeData.personnes} personnes</p>
             </div>
         </div>
       </div>
