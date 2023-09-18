b3086cdfc16fa4756bc9d1a8819f2093d73ef460
LzAkm <louiza.aitkacem@hotmail.com>
2023-09-18 22:26:15 +0200

    Modification du logo de la navbar

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,13 +1,15 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import '../Styles/Navbar.css'
-/* import logo from '../../public/img/cantina-logo.png'; */
+import '../Styles/Navbar.css';
+import { Link } from 'react-router-dom';
+import logo from '../assets/cantina-logo.png';
 
 function Navigationbar() {
     return (
         <div className='navbar'>
-            {/* <img src={ logo } alt="" /> */}
-            <h3>Homepage</h3>
+            <Link to='/'>
+                <img src={logo} className='logo' alt="logo cantina"/> 
+            </Link>
             <button className='add-btn'>ajouter une recette <FontAwesomeIcon icon={faPlus} /></button>
         </div>
     );
