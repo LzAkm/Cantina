4bf43c5ac0a0be731388def85ea964aa8fdfe165
LzAkm <louiza.aitkacem@hotmail.com>
2023-09-21 09:28:42 +0200

    Création des champs de filtrage

--- src/View/RecipesList.jsx
+++ src/View/RecipesList.jsx
@@ -9,14 +9,14 @@ function RecipesList() {
   const [recipeList, setRecipeList] = useState(recipes);
 
   useEffect(() => {
-    setRecipeList(recipes); 
+    setRecipeList(recipes);
   }, [recipes]);
 
   const handleRecipeDelete = async (recipeId) => {
     try {
       await fetchDeleteRecipe(recipeId);
       console.log('Recette supprimée avec succès.');
-      refetchRecipes(); 
+      refetchRecipes();
     } catch (error) {
       console.error('Erreur lors de la suppression de la recette :', error.message);
     }
@@ -24,6 +24,32 @@ function RecipesList() {
 
   return (
     <div className='recipes'>
+      <div className='filters'>
+        <input className='filter'
+          type='text'
+          name='title'
+          placeholder='Filtrer par titre'
+        />
+        <select className='filter-difficulty'
+          name='filter-difficulty'
+        >
+          <option value='padawan'>Padawan</option>
+          <option value='jedi'>Jedi</option>
+          <option value='maitre'>Maître</option>
+        </select>
+        <input className='filter'
+          type='number'
+          name='proportion'
+          placeholder='personnes'
+        />
+        <input className='filter'
+          type='number'
+          name='PrepTime'
+          placeholder='Temps prép. (min)'
+        />
+        <button className='filter-btn'>Appliquer</button>
+      </div>
+
       <ul className='recipes-list'>
         {loading ? (
           <div>Loading...</div>
